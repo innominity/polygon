fcce842cfcd970a0e93ab499d786947a30deee7c
leroy <leroy137.artur@gmail.com>
2024-07-17 23:22:44 +0500

    micro update

--- backend/remote_app/app_starter.py
+++ backend/remote_app/app_starter.py
@@ -1,3 +1,6 @@
+import os
+import zipfile
+from django.conf import settings
 from .models import RemoteApp, RemoteAppFileConfigType, RemoteAppTask, RemoteAppTaskFileConfig
 
 class RemoteAppBase:
@@ -24,10 +27,13 @@ class RemoteAppBase:
 class RemoteAppTaskStarter:
     """Запуск задач ПО
     """
-    
+    ROOT_APP_TASKS_PATH = os.path.join(settings.MEDIA_ROOT, 'tasks')
+
     def __init__(self, remote_app_guid):
         self.__app_guid = remote_app_guid
         self.__task_guid = self.__create_app_task(remote_app_guid)
+        self.__create_app_subfoldler()
+
 
     def __create_app_task(self, remote_app_guid):
         remote_app = RemoteApp.objects.get(guid=remote_app_guid)
@@ -35,7 +41,13 @@ class RemoteAppTaskStarter:
         remote_app_task.save()
         return remote_app_task.guid
 
-    def __create_app_subfoldler(self):
-        pass
 
-    
+    def __create_app_subfoldler(self):
+        if self.__task_guid:
+            remote_app = RemoteApp.objects.get(guid=self.__app_guid)
+            folder_app_run = os.path.join(self.ROOT_APP_TASKS_PATH, self.__task_guid)
+            with zipfile.ZipFile(remote_app.app_zip.path, 'r') as zip_ref:
+                zip_ref.extractall(folder_app_run)
+            
+    def __run_app(self):
+        pass
